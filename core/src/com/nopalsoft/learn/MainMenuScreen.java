bd6101616d7b283ec238414e29869adcd7e14802
Yayo <geramail28@gmail.com>
2022-06-09 22:39:33 +0800

    Update menu

--- core/src/com/nopalsoft/learn/MainMenuScreen.java
+++ core/src/com/nopalsoft/learn/MainMenuScreen.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.nopalsoft.learn.tutoriales.*;
-import com.nopalsoft.learn.tutoriales.learn8.Learn8;
 import com.nopalsoft.learn.utils.Learn;
 
 /**
@@ -26,12 +24,12 @@ public class MainMenuScreen extends Screens {
         Table menu = new Table();
         menu.defaults().expandY().fillY();
 
-        for (final Learn tutorial : Learn.values()) {
-            TextButton bt = new TextButton(tutorial.name, Assets.txButtonStyle);
+        for (final Learn learn : Learn.values()) {
+            TextButton bt = new TextButton(learn.name, Assets.txButtonStyle);
             bt.addListener(new ClickListener() {
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
-                    MainMenuScreen.this.game.setScreen(getScreen(tutorial));
+                    setScreen(learn);
                 }
             });
 
@@ -45,26 +43,12 @@ public class MainMenuScreen extends Screens {
         stage.addActor(scroll);
     }
 
-    private Screens getScreen(Learn learn) {
-        switch (learn) {
-            default:
-                return new Learn1(game);
-            case LEARN_2:
-                return new Learn2(game);
-            case LEARN_3:
-                return new Learn3(game);
-            case LEARN_4:
-                return new Learn4(game);
-            case LEARN_5:
-                return new Learn5(game);
-            case LEARN_6:
-                return new Learn6(game);
-            case LEARN_7:
-                return new Learn7(game);
-            case LEARN_8:
-                return new Learn8(game);
-            case LEARN_9:
-                return new Learn9(game);
+    private void setScreen(Learn learn) {
+        try {
+            Screens newScreen = learn.clazz.getConstructor(MainLearn.class).newInstance(game);
+            game.setScreen(newScreen);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
--- core/src/com/nopalsoft/learn/utils/Learn.java
+++ core/src/com/nopalsoft/learn/utils/Learn.java
@@ -1,5 +1,9 @@
 package com.nopalsoft.learn.utils;
 
+import com.nopalsoft.learn.Screens;
+import com.nopalsoft.learn.tutoriales.*;
+import com.nopalsoft.learn.tutoriales.learn8.Learn8;
+
 /**
  * Learn more about libGDX:
  * My personal blog (spanish): https://tinyurl.com/yw5hawc2
@@ -8,19 +12,21 @@ package com.nopalsoft.learn.utils;
  * @author Yayo Arellano
  */
 public enum Learn {
-    LEARN_1("The world, bodies, figures and fixtures"),
-    LEARN_2("Body Types: dynamic, static & kinematic"),
-    LEARN_3("Friction, density & restitution"),
-    LEARN_4("Forces, impulses & linear velocity"),
-    LEARN_5("Bodies & sprites"),
-    LEARN_6("Collisions"),
-    LEARN_7("Collisions with animated sprites"),
-    LEARN_8("Texture packer & animated sprites"),
-    LEARN_9("Support multiple localizations");
+    LEARN_1("The world, bodies, figures and fixtures", Learn1.class),
+    LEARN_2("Body Types: dynamic, static & kinematic", Learn2.class),
+    LEARN_3("Friction, density & restitution", Learn3.class),
+    LEARN_4("Forces, impulses & linear velocity", Learn4.class),
+    LEARN_5("Bodies & sprites", Learn5.class),
+    LEARN_6("Collisions", Learn6.class),
+    LEARN_7("Collisions with animated sprites", Learn7.class),
+    LEARN_8("Texture packer & animated sprites", Learn8.class),
+    LEARN_9("Support multiple localizations", Learn9.class);
 
     public final String name;
+    public final Class<? extends Screens> clazz;
 
-    Learn(String label) {
+    Learn(String label, Class<? extends Screens> clazz) {
         this.name = label;
+        this.clazz = clazz;
     }
 }
